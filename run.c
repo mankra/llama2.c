7f7c6ae317689e45263922cd8999ed4957d925a4
KRM <Manfred.Kral@lotterien.at>
2024-02-29 16:49:15 +0100

    ~debug

--- run.c
+++ run.c
@@ -319,7 +319,7 @@ float* forward(Transformer* transformer, int token, int pos) {
 #endif
 
         // qkv matmuls for this position
-        DBG_PRINTF("w->wq X s->xb -> s->q");
+        DBG_PRINTF("w->wq X s->xb -> s->q xb[0] %f", s->xb[0]);
         matmul(s->q, s->xb, w->wq + l*dim*dim, dim, dim);
         printVector("q", s->q, 0);
 
